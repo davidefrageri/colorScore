3b19544023a611e06c534b07ff47b53fe471801b
Davide Frageri <davide.frageri@thread.solutions>
2023-12-22 15:46:52 +0100

    fix auth0 env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        AUTH0_BASE_URL: process.env.VERCEL_URL ?? process.env.AUTH0_BASE_URL
+        AUTH0_BASE_URL: process.env.AUTH0_BASE_URL ?? process.env.VERCEL_URL
     }
 }
 
