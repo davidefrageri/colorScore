63fee1f4d0f29d309d496bbbca1478746a01ba9b
Davide Frageri <davide.frageri@thread.solutions>
2023-12-22 15:27:48 +0100

    set auth0_base_url env variable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        AUTH0_BASE_URL: process.env.VERCEL_URL ?? process.env.AUTH0_BASE_URL
+    }
+}
 
 module.exports = nextConfig
